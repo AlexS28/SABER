3bf0988ee38f2b15e7423dda2ad2a87f9752fc75
alexander <aschperb@gmail.com>
2020-10-09 23:36:58 -0700

    adding python script that allows testing of saved models during training. Models are saved in the dqn_models folder, that you'd need to add to the DQN_SABR directory

---
+++ DQN_SABR/DQN_test.py
@@ -0,0 +1,40 @@
+import torch
+from DQN_SABR.dqn_agent import Agent
+import numpy as np
+from DQN_SABR.model import QNetwork
+import gym
+import gym_dqn
+import time
+
+max_t = 100
+num_models = 100
+MAP_SIZE=10
+OBSTACLE_X = [5, 7, 3, 4, 6, 3, 5, 4, 2, 5, 4, 3]
+OBSTACLE_Y = [5, 7, 4, 3, 8, 5, 4, 5, 3, 3, 4, 3]
+robot_start = [0, 0]
+robot_goal = [0, 9]
+drone_start = [0, 0]
+drone_goal = [9, 9]
+
+env = gym.make('dqn-v0')
+env.seed(0)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+env.init(robot_start[0], robot_start[1], robot_goal[0], robot_goal[1], drone_start[0], drone_start[1], drone_goal[0],
+         drone_goal[1], MAP_SIZE, OBSTACLE_X, OBSTACLE_Y)
+
+for i in range(0, num_models):
+
+    model = QNetwork(state_size=(len(OBSTACLE_X)+1)*2, action_size=81, seed=0).to(device)
+    model.load_state_dict(torch.load('dqn_models{}checkpoint{}.pth'.format('/', i)))
+    state = env.reset()
+
+    for t in range(max_t):
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        action_values = model(state)
+        action = np.argmax(action_values.cpu().data.numpy())
+        next_state, _, done = env.step(action)
+        state = next_state
+        if (env.xr==env.gxr and env.yr==env.gyr) and (env.xd==env.gxd and env.yd==env.gyd):
+            break
+        env.render()
+        time.sleep(.5)
