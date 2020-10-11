2530f115ab94d604a9d0d11bbae4e645930a157e
alexander <aschperb@gmail.com>
2020-10-10 23:43:03 -0700

    adding completed uav, ugv, and their combination, SABR in integration

---
+++ src/sabr_pkg/SABR.py
@@ -0,0 +1,134 @@
+from src.SABR.src.sabr_pkg.SMPC_ugv import *
+from src.SABR.src.sabr_pkg.SMPC_uav import *
+
+# This code runs the 'Synchronous and Autonomous Robots in Uncertain Environments with Reinforcement Learning' (SABR)
+# algorithm. It combines a stochastic MPC for a multi-agent (UAV-UGV) system, which infers future uncertainty covariances
+# using a recurrent neural network (trained on filter estimations provided by a particle-filter or visual-inertial
+# SLAM system----depending if the robot is equipped with a LiDAR or RGB-D camera configuration respectively).
+# Lastly, to provide global # target locations for both robots, is done using a DQN, which is rewarded based on whether
+# both robots reach their # final goal destination, if their SMPC controller succeeds in finding solutions, and
+# if uncertainty is always below a specified threshold.
+
+# initialize obstacles
+obs = {1: {'vertices': [[3.9, 4], [4, 6], [6, 6.1], [5.9, 4.1]], 'a': [], 'slopes': [], 'intercepts': [],
+               'polygon_type': 4, 'risk': 0.1}}
+obs.update(
+    {2: {'vertices': [[6, 5], [7, 7], [8, 5.2]], 'a': [], 'slopes': [], 'intercepts': [], 'polygon_type': 3,
+             'risk': 0.4}})
+obs.update(
+    {3: {'vertices': [[2, 2]], 'size': 0.5, 'polygon_type': 1, 'risk': 0.4}})
+
+# initialize prediction horizon and discretized time, and whether to animate
+dT = 0.1
+mpc_horizon = 10
+animate = True
+
+# initialize SMPC parameters for the UGV
+curr_posUGV = np.array([0, 0, 0]).reshape(3,1)
+goal_pointsUGV = [[10, 8, 0], [10, 0, -pi/2]]
+robot_size = 0.5
+lb_state = np.array([[-20], [-20], [-2*pi]], dtype=float)
+ub_state = np.array([[20], [20], [2*pi]], dtype=float)
+lb_control = np.array([[-1.5], [-np.pi/2]], dtype=float)
+ub_control = np.array([[1.5], [np.pi/2]], dtype=float)
+Q = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+R_init = np.array([[1, 0, 0], [0, 1, 0] ,[0, 0, 0.001]])
+angle_noise_r1 = 0.0
+angle_noise_r2 = 0.0
+relative_measurement_noise_cov = np.array([[0.0,0], [0,0.0]])
+maxComm_distance = -10
+failure_count = 0
+
+SMPC_UGV = SMPC_UGV_Planner(dT, mpc_horizon, curr_posUGV, robot_size, lb_state,
+                            ub_state, lb_control, ub_control, Q, R_init, angle_noise_r1, angle_noise_r2,
+                            relative_measurement_noise_cov, maxComm_distance, obs, animate)
+
+# initialize SMPC parameters for the UAV
+curr_posUAV = np.array([0,0,0,0,0,0,0,0,4,0]).reshape(10,1)
+goal_pointsUAV = [[11,0,0,0,11,0,0,0,4,0], [0,0,0,0,0,0,0,0,10,0]]
+robot_size = 0.5
+vel_limit = 2
+lb_state = np.array(
+        [[-10**10], [-vel_limit], [-10**10], [-10**10], [-10**10], [-vel_limit], [-10**10], [-10**10], [-vel_limit],
+         [-10**10]], dtype=float)
+ub_state = np.array(
+        [[10**10], [vel_limit], [10**10], [10**10], [10**10], [vel_limit], [10**10], [10**10], [10**10], [vel_limit]],
+        dtype=float)
+lb_control = np.array([[-1], [-1], [-1]], dtype=float)
+ub_control = np.array([[1], [1], [1]], dtype=float)
+
+Q = np.array([[1,0,0,0,0,0,0,0,0,0],
+                          [0,1,0,0,0,0,0,0,0,0],
+                          [0,0,1,0,0,0,0,0,0,0],
+                           [0,0,0,1,0,0,0,0,0,0],
+                           [0,0,0,0,1,0,0,0,0,0],
+                           [0,0,0,0,0,1,0,0,0,0],
+                           [0,0,0,0,0,0,1,0,0,0],
+                           [0,0,0,0,0,0,0,1,0,0],
+                           [0,0,0,0,0,0,0,0,1,0],
+                           [0,0,0,0,0,0,0,0,0,1]])
+
+R = np.array([[0.001, 0, 0], [0, 0.001, 0], [0, 0, 0.001]])
+
+# if the UAV and UGV are to be animated in the same screen, multi_agent must be set to true
+SMPC_UAV = SMPC_UAV_Planner(dT, mpc_horizon, curr_posUAV, lb_state,
+                            ub_state, lb_control, ub_control, Q, R, robot_size, obs, animate, multi_agent=True)
+
+goal_posUGV = np.array(goal_pointsUGV[0])
+SMPC_UGV.opti.set_value(SMPC_UGV.r1_goal, goal_posUGV)
+
+goal_posUAV = np.array(goal_pointsUAV[0])
+SMPC_UAV.opti.set_value(SMPC_UAV.r_goal, goal_posUAV)
+
+indexUGV = 1
+indexUAV = 1
+
+while m.sqrt((curr_posUGV[0] - goal_posUGV[0]) ** 2 + (curr_posUGV[1] - goal_posUGV[1]) ** 2) > 0.5 or m.sqrt((curr_posUAV[0]
+            - goal_posUAV[0]) ** 2 + (curr_posUAV[4] - goal_posUAV[4]) ** 2 + (curr_posUAV[8] - goal_posUAV[8])**2) > 0.5:
+
+    # find SMPC solution for UGV
+    try:
+        solUGV = SMPC_UGV.opti.solve()
+        x = solUGV.value(SMPC_UGV.X)[:, 1]
+        u = solUGV.value(SMPC_UGV.U[:, SMPC_UGV.N - 1])
+        SMPC_UGV.check_obstacles(x[0:2])
+    except:
+        u = solUGV.value(SMPC_UGV.U[:, 0])
+        u[1] = 0
+        x = SMPC_UGV.next_state_nominal(curr_posUGV, u)
+        print('WARNING: Solver has failed for UGV, using previous control value for next input')
+    curr_posUGV = np.array(x).reshape(3, 1)
+    SMPC_UGV.opti.set_value(SMPC_UGV.r1_pos, x)
+
+    # find SMPC solution for UAV
+    try:
+        solUAV = SMPC_UAV.opti.solve()
+        x = solUAV.value(SMPC_UAV.X)[:, 1]
+        u = solUAV.value(SMPC_UAV.U)[:, SMPC_UAV.N-1]
+        SMPC_UAV.check_obstacles([curr_posUAV[0], curr_posUAV[4]])
+    except:
+        u = solUAV.value(SMPC_UAV.U[:,0])
+        x = SMPC_UAV.next_state_nominal(curr_posUAV, u)
+        print('WARNING: Solver has failed for UAV, using previous control value for next input')
+    curr_posUAV = np.array(x).reshape(10, 1)
+    SMPC_UAV.opti.set_value(SMPC_UAV.r_pos, x)
+
+    dist_goalUGV = m.sqrt((curr_posUGV[0] - goal_posUGV[0]) ** 2 + (curr_posUGV[1] - goal_posUGV[1]) ** 2)
+    dist_goalUAV = m.sqrt((curr_posUAV[0]
+            - goal_posUAV[0]) ** 2 + (curr_posUAV[4] - goal_posUAV[4]) ** 2 + (curr_posUAV[8] - goal_posUAV[8])**2)
+
+    if dist_goalUGV < 0.5 and indexUGV <= len(goal_pointsUGV)-1:
+        goal_posUGV = np.array(goal_pointsUGV[indexUGV])
+        SMPC_UGV.opti.set_value(SMPC_UGV.r1_goal, goal_posUGV)
+        indexUGV += 1
+
+    if dist_goalUAV < 0.5 and indexUAV <= len(goal_pointsUAV)-1:
+        goal_posUAV = np.array(goal_pointsUAV[indexUAV])
+        SMPC_UAV.opti.set_value(SMPC_UAV.r_goal, goal_posUAV)
+        indexUAV += 1
+
+    SMPC_UGV.animate(curr_posUGV)
+    SMPC_UAV.animate_multi_agents(SMPC_UGV.ax, curr_posUAV)
+
+    plt.show()
+    plt.pause(0.001)
---
+++ src/sabr_pkg/SMPC_uav.py
@@ -0,0 +1,359 @@
+"""FILE CREATED BY: Alexander Schperberg, aschperb@gmail.com
+Copyright by RoMeLa (Robotics and Mechanisms Laboratory, University of California, Los Angeles)"""
+
+# This file provides a simple model predictive controller for a unmanned aerial vehicle for data collection
+# purposes that moves a aerial to several user-specified goal locations
+
+from casadi import *
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.art3d as art3d
+from matplotlib.patches import Circle
+import matplotlib.pyplot as plt
+import math as m
+import control
+
+class SMPC_UAV_Planner():
+
+    def __init__(self, dT, mpc_horizon, curr_pos, lb_state,
+                 ub_state, lb_control, ub_control, Q, R, robot_size, obs, animate, multi_agent):
+
+        # initialize Optistack class
+        self.opti = casadi.Opti()
+        # dt = discretized time difference
+        self.dT = dT
+        # mpc_horizon = number of time steps for the mpc to look ahead
+        self.N = mpc_horizon
+        # robot_size = input a radius value, where the corresponding circle represents the size of the robot
+        self.robot_size = robot_size
+        # if the uav is to be animated along the same graph as the ugv, then put multi_agent to true
+        self.multi_agent = multi_agent
+        # lower_bound_state = numpy array corresponding to the lower limit of the robot states, e.g.
+        # lb_state = np.array([[-20], [-20], [-pi], dtype=float), the same for the upper limit (ub). Similar symbolic
+        # representation for the controls (lb_control and ub_control) as well
+        self.lb_state = lb_state
+        self.ub_state = ub_state
+        self.lb_control = lb_control
+        self.ub_control = ub_control
+        # initialize obstacles
+        self.obs = obs
+        # distance to obstacle to be used as constraints
+        self.max_obs_distance = 5
+        # Q and R diagonal matrices, used for the MPC objective function, Q is 3x3, R is 4x4 (first 2 diagonals
+        # represent the cost on linear and angular velocity, the next 2 diagonals represent cost on state slack,
+        # and terminal slack respectively. The P diagonal matrix represents the cost on the terminal constraint.
+        self.Q = Q
+        self.R = R
+        # initialize cost on slack
+        self.slack_cost = 10000
+        # initialize discretized state matrices A and B
+        A1 = 0.7969
+        A2 = 0.02247
+        A3 = -1.7976
+        A4 = 0.9767
+        B1 = 0.01166
+        B2 = 0.9921
+        g = 9.81
+        KT = 0.91
+        m = 1.3
+
+        self.A = np.array([[1, self.dT, (g*self.dT**2)/2, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 1, g*self.dT, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, A1, A2, 0, 0, 0, 0, 0, 0],
+                          [0, 0, A3, A4, 0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 1, self.dT, (g*self.dT**2)/2, 0, 0, 0],
+                           [0,0,0,0,0,1,g*self.dT,0,0,0],
+                           [0,0,0,0,0,0,A1,A2,0,0],
+                           [0,0,0,0,0,0,A3,A4,0,0],
+                           [0,0,0,0,0,0,0,0,1,self.dT],
+                           [0,0,0,0,0,0,0,0,0,1]])
+
+        self.B = np.array([[0,0,0], [0,0,0], [B1,0,0], [B2,0,0], [0,0,0],
+                           [0,0,0], [0,B1,0],[0,B2,0],[0,0,(-KT*self.dT**2)/(m*2)],
+                           [0,0,(-KT*self.dT)/m]])
+        self.P, _, _ = control.dare(self.A, self.B, self.Q, self.R)
+        # TODO: adding the C matrix does not work, potentially a problem with equations
+        #self.C = np.array([[0],[0],[0],[0],[0],[0],[0],[0],[0.0031], [0.2453]])
+        # Current covariance of robot
+        self.r1_cov_curr = np.array([[0.01 + self.robot_size, 0], [0, 0.01 + self.robot_size]])
+        # initialize robot's current position
+        self.curr_pos = curr_pos
+        self.initVariables()
+        # initialize parameters for animation
+        if animate:
+            if not self.multi_agent:
+                plt.ion()
+                fig = plt.figure()
+                fig.canvas.mpl_connect('key_release_event',
+                                   lambda event: [exit(0) if event.key == 'escape' else None])
+                self.ax = fig.add_subplot(111, projection='3d')
+                self.ax = Axes3D(fig)
+            u = np.linspace(0, 2 * np.pi, 100)
+            v = np.linspace(0, np.pi, 100)
+            self.x_fig = np.outer(self.robot_size * np.cos(u), np.sin(v))
+            self.y_fig = np.outer(self.robot_size * np.sin(u), np.sin(v))
+            self.z_fig = np.outer(self.robot_size * np.ones(np.size(u)), np.cos(v))
+
+    def initVariables(self):
+
+        # initialize x, y, and theta state variables
+        self.X = self.opti.variable(10, self.N+1)
+        self.x_pos = self.X[0,:]
+        self.x_vel = self.X[1,:]
+        self.th1 = self.X[2,:]
+        self.th1_vel = self.X[3,:]
+        self.y_pos = self.X[4,:]
+        self.y_vel = self.X[5,:]
+        self.th2 = self.X[6,:]
+        self.th2_vel = self.X[7,:]
+        self.z_pos = self.X[8,:]
+        self.z_vel = self.X[9,:]
+
+        # initialize, linear and angular velocity control variables (v, and w), and repeat above procedure
+        self.U = self.opti.variable(3, self.N)
+        self.u1 = self.U[0, :]
+        self.u2 = self.U[1, :]
+        self.u3 = self.U[2, :]
+
+        # initalize slack variable
+        self.slack = self.opti.variable(1, self.N+1)
+
+        # initialize the current robot pos
+        self.r_pos = self.opti.parameter(10, 1)
+        self.opti.set_value(self.r_pos, self.curr_pos)
+
+        # initialize the goal point (x, y and th current position)
+        self.r_goal = self.opti.parameter(10, 1)
+
+        # initialize the objective function
+        self.obj()
+
+    def obj(self):
+        self.objFunc = 0
+        for k in range(0, self.N-1):
+            con = self.U[:, k]
+            st = self.X[:, k + 1]
+
+            self.objFunc = self.objFunc + mtimes(mtimes((st - self.r_goal).T, self.Q), st - self.r_goal) + \
+                           mtimes(mtimes(con.T, self.R), con) + self.slack[:,k+1]*self.slack_cost
+
+            st = self.X[:, self.N]
+
+            self.objFunc = self.objFunc + mtimes(mtimes((st - self.r_goal).T, self.P), st - self.r_goal) + self.slack[:, self.N] * self.slack_cost
+
+        self.init_constraints()
+
+        # initialize the objective function into the solver
+        self.opti.minimize(self.objFunc)
+
+    def init_constraints(self):
+        # constrain the current state, and bound current and future states by their limits
+        self.opti.subject_to(self.X[:, 0] == self.r_pos)
+        self.opti.subject_to(self.opti.bounded(self.lb_state, self.X, self.ub_state))
+        self.opti.subject_to(self.opti.bounded(self.lb_control, self.U, self.ub_control))
+
+        # constrain the slack variable
+        self.opti.subject_to(self.slack >= 0)
+
+        # initiate multiple shooting constraints
+        for k in range(0, self.N):
+            next_state = self.next_state_nominal(self.X[:, k], self.U[:, k])
+            self.opti.subject_to(self.X[:, k + 1] == next_state)
+
+        # initialize chance constraints
+        self.chance_constraints()
+        # set solver
+        self.pre_solve()
+
+    def chance_constraints(self):
+
+        # Using chance constraints on circular obstacles
+        self.obs_index = []
+        for i in range(1, len(self.obs) + 1):
+            if self.obs[i]['polygon_type'] == 1:
+                self.obs_index.append(self.obs[i]['polygon_type'])
+
+        self.cc = self.opti.parameter(sum(self.obs_index), self.N + 1)
+        self.opti.set_value(self.cc, 1)
+
+        self.switch_obs = self.opti.parameter(len(self.obs_index), 1)
+        self.opti.set_value(self.switch_obs, 0)
+
+        for i in range(1, len(self.obs) + 1):
+            iter = 0
+            if self.obs[i]['polygon_type'] == 1:
+                a = np.array([1, 1]).reshape(2, 1)
+                r = self.obs[i]['risk']
+                center = self.obs[i]['vertices'][0]
+                size = self.obs[i]['size']
+                self.opti.set_value(self.cc[iter, :],
+                                    np.sqrt(np.dot(np.dot(2 * np.transpose(a), self.r1_cov_curr), a)) * erfinv(
+                                        (1 - 2 * r)))
+                for k in range(0, self.N + 1):
+                    dist = -1 * self.distance_pt_circle(center, [self.X[0, k], self.X[4, k]], size, self.robot_size)+self.cc[iter] - self.slack[0,k]
+                    self.opti.subject_to(dist*self.switch_obs[iter] <= 0)
+                iter += 1
+
+    def check_obstacles(self, curr_pos):
+        iter = 0
+        for i in range(1, len(self.obs)+1):
+
+            if self.obs[i]['polygon_type'] == 1:
+                center = self.obs[i]['vertices'][0]
+                size = self.obs[i]['size']
+                dist = self.distance_pt_circle(center, curr_pos, size, self.robot_size)
+                if dist <= self.max_obs_distance:
+                    a = np.array([1, 1]).reshape(2, 1)
+                    r = self.obs[i]['risk']
+
+                    for k in range(0, self.N + 1):
+                        # self.r1_cov_curr[0,:] = self.r1_pos_cov[0:2,k]
+                        # self.r1_cov_curr[1, :] = self.r1_pos_cov[2:4,k]
+                        self.opti.set_value(self.cc[iter, k],
+                                    np.sqrt(np.dot(np.dot(2 * np.transpose(a), self.r1_cov_curr), a)) * erfinv(
+                                        (1 - 2 * r)))
+                        self.opti.set_value(self.cc[iter,k], 0)
+                        self.opti.set_value(self.switch_obs[iter], 1)
+                        iter += 1
+                        break
+                else:
+                    self.opti.set_value(self.switch_obs[iter], 0)
+                    iter += 1
+                    break
+
+    def next_state_nominal(self, x, u):
+        next_state = mtimes(self.A, x) + mtimes(self.B, u) #+ self.C
+        return next_state
+
+    def pre_solve(self):
+        # TODO: As with the UGV, add line constraints for the UAV, this requires bonmin solver. For now, ipopt is used
+        # TODO: as only circular constraints are considered for the UAV
+
+        """
+        opts = {'bonmin.warm_start': 'interior_point', 'discrete': OT_Boolvector, 'error_on_fail': True,
+                'bonmin.time_limit': 0.5,
+                'bonmin.acceptable_obj_change_tol': 1e40, 'bonmin.acceptable_tol': 1e-1}
+
+        # create the solver
+        self.opti.solver('bonmin', opts)
+        """
+
+        # set solver options for ipopt (nonlinear programming)
+        opts = {'ipopt': {'max_iter': 1000, 'print_level': 0, 'acceptable_tol': 10**-9,
+                      'acceptable_obj_change_tol': 10**-7}}
+        opts.update({'print_time': 0})
+        # create solver
+        self.opti.solver('ipopt', opts)
+
+    def distance_pt_circle(self, center, point, obs_size, robot_size):
+        # calculates the distance between the outerboundary of an obstacle and the outerboundary of the robot
+        dist = np.sqrt((point[0] - center[0]) ** 2 + (point[1] - center[1]) ** 2) - robot_size - obs_size
+        return dist
+
+    def animate(self, curr_pos):
+        plt.cla()
+        plt.xlim(0, 10)
+        plt.ylim(0, 10)
+        self.ax.set_zlim(0, 10)
+        # graph robot as a round sphere for simplicity
+        self.ax.plot_surface(self.x_fig + curr_pos[0], self.y_fig + curr_pos[4], self.z_fig + curr_pos[8],
+                             rstride=4, cstride=4, color='b')
+
+        for i in range(1, len(self.obs) + 1):
+            if self.obs[i]['polygon_type'] == 1:
+                center = self.obs[i]['vertices'][0]
+                size = self.obs[i]['size']
+                q = Circle((center[0], center[1]), size, color='red')
+                self.ax.add_patch(q)
+                art3d.pathpatch_2d_to_3d(q, z=0, zdir="z")
+
+    def animate_multi_agents(self, ax, curr_pos):
+        ax.set_zlim(0, 10)
+        # graph robot as a round sphere for simplicity
+        ax.plot_surface(self.x_fig + curr_pos[0], self.y_fig + curr_pos[4], self.z_fig + curr_pos[8],
+                             rstride=4, cstride=4, color='b')
+        x_togo = 2 * np.cos(0)
+        y_togo = 2 * np.sin(0)
+
+        x_togo2 = 2 * np.cos(pi/2)
+        y_togo2 = 2 * np.sin(pi/2)
+
+        x_togo3 = 2 * np.cos(pi)
+        y_togo3 = 2 * np.sin(pi)
+
+        x_togo4 = 2 * np.cos(3*pi/2)
+        y_togo4 = 2 * np.sin(3*pi/2)
+
+        # graph direction of the robot heading
+        ax.quiver(curr_pos[0], curr_pos[4], curr_pos[8], x_togo, y_togo, 0, color='red', alpha=.8, lw=2)
+        ax.quiver(curr_pos[0], curr_pos[4], curr_pos[8], x_togo2, y_togo2, 0, color='red', alpha=.8, lw=2)
+        ax.quiver(curr_pos[0], curr_pos[4], curr_pos[8], x_togo3, y_togo3, 0, color='red', alpha=.8, lw=2)
+        ax.quiver(curr_pos[0], curr_pos[4], curr_pos[8], x_togo4, y_togo4, 0, color='red', alpha=.8, lw=2)
+
+"""
+if __name__ == '__main__':
+
+    # initialize all required variables for the SMPC solver
+    dT = 0.1
+    mpc_horizon = 10
+    # x, vel_x, th1, th1_vel, y, vel_y, th2, th2_vel, z, z_vel
+    curr_pos = np.array([0,0,0,0,0,0,0,0,0,0]).reshape(10,1)
+    goal_points = [[11,0,0,0,11,0,0,0,0,0], [0,0,0,0,0,0,0,0,10,0]]
+
+    robot_size = 0.5
+    #lb_state = np.array([[-20], [-5], [-10*(pi/180)], [-50*pi/180],[-20], [-5], [-10*(pi/180)], [-50*pi/180],[-20],[-5]], dtype=float)
+    #ub_state = np.array([[20], [5], [10*(pi/180)], [50*pi/180],[20], [5], [10*(pi/180)], [50*pi/180],[20],[5]], dtype=float)
+    #lb_control = np.array([[-10*pi/180], [-10*pi/180], [-10*pi/180]], dtype=float)
+    #ub_control = np.array([[10*pi/180], [10*pi/180], [10*pi/180]], dtype=float)
+
+    # only constraints on the states will be the velocity (this provided the most stability)
+    vel_limit = 2
+    lb_state = np.array(
+        [[-10**10], [-vel_limit], [-10**10], [-10**10], [-10**10], [-vel_limit], [-10**10], [-10**10], [-vel_limit],
+         [-10**10]], dtype=float)
+    ub_state = np.array(
+        [[10**10], [vel_limit], [10**10], [10**10], [10**10], [vel_limit], [10**10], [10**10], [10**10], [vel_limit]],
+        dtype=float)
+    lb_control = np.array([[-1], [-1], [-1]], dtype=float)
+    ub_control = np.array([[1], [1], [1]], dtype=float)
+
+
+    Q = np.array([[1,0,0,0,0,0,0,0,0,0],
+                          [0,1,0,0,0,0,0,0,0,0],
+                          [0,0,1,0,0,0,0,0,0,0],
+                           [0,0,0,1,0,0,0,0,0,0],
+                           [0,0,0,0,1,0,0,0,0,0],
+                           [0,0,0,0,0,1,0,0,0,0],
+                           [0,0,0,0,0,0,1,0,0,0],
+                           [0,0,0,0,0,0,0,1,0,0],
+                           [0,0,0,0,0,0,0,0,1,0],
+                           [0,0,0,0,0,0,0,0,0,1]])
+
+    R = np.array([[0.001, 0, 0], [0, 0.001, 0], [0, 0, 0.001]])
+
+    obs = {1: {'vertices': [[3.9, 4], [4, 6], [6, 6.1], [5.9, 4.1]], 'a': [], 'slopes': [], 'intercepts': [],
+               'polygon_type': 4, 'risk': 0.1}}
+    obs.update(
+        {2: {'vertices': [[6, 5], [7, 7], [8, 5.2]], 'a': [], 'slopes': [], 'intercepts': [], 'polygon_type': 3,
+             'risk': 0.4}})
+    obs.update(
+        {3: {'vertices': [[4, 4.1]], 'size': 0.7, 'polygon_type': 1, 'risk': 0.4}})
+
+    animate = True
+
+    SMPC = SMPC_UAV_Planner(dT, mpc_horizon, curr_pos, lb_state,
+                            ub_state, lb_control, ub_control, Q, R, robot_size, obs, animate)
+
+    for i in range(0, len(goal_points)):
+
+        goal_pos = np.array(goal_points[i])
+        SMPC.opti.set_value(SMPC.r_goal, goal_pos)
+
+        while m.sqrt((curr_pos[0] - goal_pos[0]) ** 2 + (curr_pos[4] - goal_pos[4]) ** 2 + (curr_pos[8] - goal_pos[8])**2) > 0.5:
+            sol = SMPC.opti.solve()
+            x = sol.value(SMPC.X)[:, 1]
+            curr_pos = np.array(x).reshape(10, 1)
+            SMPC.opti.set_value(SMPC.r_pos, x)
+            SMPC.check_obstacles([curr_pos[0], curr_pos[4]])
+            SMPC.animate(curr_pos)
+"""
